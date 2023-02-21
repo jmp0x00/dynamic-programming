fe272588092155c29e52b7da74498312aac691e7
vadim.kiryukhin <vadim.kiryukhin@ringcentral.com>
2023-02-21 23:29:45 +0400

    Add knapsack tabulation

--- src/main/scala/knapsack/Knapsack.scala
+++ src/main/scala/knapsack/Knapsack.scala
@@ -1,6 +1,6 @@
 package knapsack
 
-import zio._
+import zio.{System => _, _}
 import zio.Console._
 
 object Knapsack extends ZIOAppDefault {
@@ -30,24 +30,48 @@ object Knapsack extends ZIOAppDefault {
     getMaxProfitRecursive(items.toArray, capacity)
 
   def getMaxProfitRecursive(items: Array[Item], capacity: Int): Int = {
-    val memoize = Array.fill[Option[Int]](items.length, capacity + 1)(None)
+    if (capacity <= 0 || items.isEmpty) {
+      0
+    } else {
+      val dp = Array.ofDim[Int](items.length, capacity + 1)
 
-    def get(currentCapacity: Int, currentIndex: Int): Int =
-      if (currentCapacity <= 0 || currentIndex >= items.length) {
-        0
-      } else if (memoize(currentIndex)(currentCapacity).nonEmpty) {
-        memoize(currentIndex)(currentCapacity).get
-      } else {
-        val skip   = get(currentCapacity, currentIndex + 1)
-        val select =
-          if (items(currentIndex).weight <= currentCapacity)
-            items(currentIndex).profit + get(currentCapacity - items(currentIndex).weight, currentIndex + 1)
-          else
-            0
-        memoize(currentIndex).update(currentCapacity, Some(skip max select))
-        memoize(currentIndex)(currentCapacity).get
+      for (i <- items.indices) {
+        dp(i).update(0, 0)
       }
 
-    get(capacity, 0)
+      for (c <- 0 to capacity) {
+        if (items(0).weight <= c)
+          dp(0).update(c, items(0).profit)
+      }
+
+      for (i <- 1 until items.length) {
+        for (c <- 1 to capacity) {
+          val profit1 = if (items(i).weight <= c) items(i).profit + dp(i - 1)(c - items(i).weight) else 0
+          val profit2 = dp(i - 1)(c)
+          dp(i).update(c, profit1 max profit2)
+        }
+      }
+
+      printSelectedItems(dp, items, capacity)
+
+      dp(items.length - 1)(capacity)
+    }
+  }
+
+  private def printSelectedItems(dp: Array[Array[Int]], items: Array[Item], capacity: Int): Unit = {
+    System.out.print("Selected weights:")
+    var currentCapacity = capacity
+    var totalProfit = dp(items.length - 1)(capacity)
+    for (i <- (items.length - 1) until 0 by -1) {
+      if (totalProfit != dp(i-1)(currentCapacity)) {
+        System.out.print(" " + items(i).weight)
+        currentCapacity -= items(i).weight
+        totalProfit -= items(i).profit
+      }
+    }
+
+    if (totalProfit != 0)
+      System.out.print(" " + items(0).weight)
+    System.out.println("")
   }
 }
